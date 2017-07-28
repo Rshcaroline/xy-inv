709af4bc7297b1e86e2e201d9223bcb88dc27c08
shihan.ran <shihan.ran@xy-inv.com>
2017-07-28 17:43:40 +0800

    ''

--- Patent/dbwork.py
+++ Patent/dbwork.py
@@ -4,8 +4,14 @@ import pandas as pd
 sqlserver_url = "mssql+pymssql://shihan.ran:Caroline970505@192.168.66.12:1433"
 engine = create_engine(sqlserver_url)
 
-sql_query = '''SELECT [InnerCode],[CompanyCode],[SecuCode] FROM [Public].[dbo].[QL_ASharesList]
-  order by InnerCode '''
+sql_query = '''
+SELECT TOP (1000) 
+      [InnerCode]
+      ,[SecuCode]
+      ,[ChiName]
+      ,[ChiNameAbbr]
+  FROM [JYDB].[dbo].[SecuMain]
+  '''
 
 dic = pd.read_sql_query(sql_query, engine)
 
