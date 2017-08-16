4c4d1de817cc618ca5b989fb1a10de397618928c
Rshcaroline <rshcaroline@163.com>
2017-08-16 17:37:41 +0800

    'update'

--- Visualization/graph.py
+++ Visualization/graph.py
@@ -10,6 +10,8 @@ reader = csv.reader(open("data.csv"))
 stm = []
 sbp = []
 sap = []
+sbv = []
+sav = []
 
 for contradiction, time, lastprice, volume, bp1, bv1, bp2, bv2, bp3, bv3, \
     bp4, bv4, bp5, bv5, bp6, bv6, bp7, bv7, bp8, bv8, bp9, bv9, bp10, bv10, \
@@ -19,7 +21,9 @@ for contradiction, time, lastprice, volume, bp1, bv1, bp2, bv2, bp3, bv3, \
     if contradiction == 'ag1712':
         tm = [time, time, time, time, time, time, time, time, time, time]
         bp = [bp1, bp2, bp3, bp4, bp5, bp6, bp7, bp8, bp9, bp10]
+        bv = [bv1, bv2, bv3, bv4, bv5, bv6, bv7, bv8, bv9, bv10]
         ap = [ap1, ap2, ap3, ap4, ap5, ap6, ap7, ap8, ap9, ap10]
+        av = [av1, av2, av3, av4, av5, av6, av7, av8, av9, av10]
 
         bp = [np.NaN if x == '0.00' else x for x in bp]
         ap = [np.NaN if x == '0.00' else x for x in ap]
@@ -27,6 +31,8 @@ for contradiction, time, lastprice, volume, bp1, bv1, bp2, bv2, bp3, bv3, \
         stm.append(tm)
         sbp.append(bp)
         sap.append(ap)
+        sbv.append(bv)
+        sav.append(av)
 
         plt.title("I'm a scatter diagram.")
         # plt.xlim(xmax= 2190, xmin=time)
@@ -38,9 +44,13 @@ for contradiction, time, lastprice, volume, bp1, bv1, bp2, bv2, bp3, bv3, \
         for i in range(len(stm)):
             print(np.shape(stm[i]), np.shape(sbp[i]), np.shape(sap[i]))
             print(stm[i], sbp[i], sap[i])
-            typebp = axes.scatter(stm[i], sbp[i], s=20, c='blue', alpha=0.7, marker='^')
-            typeap = axes.scatter(stm[i], sap[i], s=50, c='red', alpha=0.7, marker='v')
-
+            axes.scatter(stm[i], sbp[i], s=20, c='blue', alpha=0.7, marker='^')
+            for j in range(10):
+                axes.text(stm[i][j], sbp[i][j],sbv[i][j],family='monospace',fontsize=10,color='blue')
+            axes.scatter(stm[i], sap[i], s=50, c='red', alpha=0.7, marker='v')
+            for j in range(10):
+                axes.text(stm[i][j], sap[i][j],sav[i][j],family='monospace',fontsize=10,color='red')
         plt.show()
+
     else:
         continue
