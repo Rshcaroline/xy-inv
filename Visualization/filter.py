1cc3c54bb23c155692724dba003abb062396617c
Rshcaroline <rshcaroline@163.com>
2017-08-16 15:12:38 +0800

    'update'

---
+++ Visualization/filter.py
@@ -0,0 +1,18 @@
+#encoding = utf-8#
+
+import os, sys, string
+
+ifn = r"data.csv"
+ofn = r"Output.txt"
+
+infile = open(ifn,'rb')
+outfile = open(ofn,'wb')
+
+for eachline in infile.readlines():
+    lines = eachline.split(', ')
+    for temp in lines:
+        print(temp)
+        outfile.write(temp+'\n')
+
+infile.close
+outfile.close
--- Visualization/graph.py
+++ Visualization/graph.py
@@ -16,30 +16,17 @@ for contradiction, time, lastprice, volume, bp1, bv1, bp2, bv2, bp3, bv3, \
     ap1, av1, ap2, av2, ap3, av3, ap4, av4, ap5, av5, ap6, av6, \
     ap7, av7, ap8, av8, ap9, av9, ap10, av10 in reader:
 
-    if contradiction == 'ag1707':
+    if contradiction == 'ag1712':
         tm = [time, time, time, time, time, time, time, time, time, time]
         bp = [bp1, bp2, bp3, bp4, bp5, bp6, bp7, bp8, bp9, bp10]
         ap = [ap1, ap2, ap3, ap4, ap5, ap6, ap7, ap8, ap9, ap10]
 
-        bp = [np.NaN if x == 0 else x for x in bp]
-        ap = [np.NaN if x == 0 else x for x in ap]
+        bp = [np.NaN if x == '0.00' else x for x in bp]
+        ap = [np.NaN if x == '0.00' else x for x in ap]
 
         stm.append(tm)
         sbp.append(bp)
         sap.append(ap)
-
-        # print(len(stm), len(sbp), len(sap))
-        # print(stm, sbp, sap)
-
-        '''
-        axes = plt.subplot(111)
-        type1 = axes.scatter(tm, bp, s=20, c='red')
-        type3 = axes.scatter(tm, ap, s=50, c='blue')
-    
-        plt.xlabel('time')
-        plt.ylabel('price')
-        plt.show()
-        '''
 
         plt.title("I'm a scatter diagram.")
         # plt.xlim(xmax= 2190, xmin=time)
@@ -51,10 +38,9 @@ for contradiction, time, lastprice, volume, bp1, bv1, bp2, bv2, bp3, bv3, \
         for i in range(len(stm)):
             print(np.shape(stm[i]), np.shape(sbp[i]), np.shape(sap[i]))
             print(stm[i], sbp[i], sap[i])
-            typebp = axes.scatter(stm[i], sbp[i], s=20, c='blue')
-            typeap = axes.scatter(stm[i], sap[i], s=50, c='red')
-            # plt.plot(tm, bp, 'ro', c='blue')
-            # plt.plot(tm, ap, 'ro', c='red')
+            typebp = axes.scatter(stm[i], sbp[i], s=20, c='blue', alpha=0.7, marker='^')
+            typeap = axes.scatter(stm[i], sap[i], s=50, c='red', alpha=0.7, marker='v')
+
         plt.show()
     else:
         continue
